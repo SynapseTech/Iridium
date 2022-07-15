da161b7596d97d1ecff7ae9c547e23588ed0b41a
Liz Ainslie <lizainslie16@gmail.com>
2022-07-15 08:07:31 -0700

    Add demo code

---
+++ packages/vue/demo/Demo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import IrToolbar from '../src/components/IrToolbar.vue';
+import IrToolbarItem from '../src/components/IrToolbarItem.vue';
+</script>
+
+<template>
+  <ir-toolbar>
+    <ir-toolbar-item active>Home</ir-toolbar-item>
+    <ir-toolbar-item>About</ir-toolbar-item>
+  </ir-toolbar>
+</template>
+
+
+<style scoped></style>
---
+++ packages/vue/demo/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import Demo from './Demo.vue';
+
+import '@iridium-design/styles/dist/iridium.css';
+
+const app = createApp(Demo);
+app.mount('#app');
