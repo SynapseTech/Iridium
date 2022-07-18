6e010c4f9d6e5c1a40661ff3e5557eeadf1c903a
Liz Ainslie <lizainslie16@gmail.com>
2022-07-18 08:01:10 -0700

    Add container styles and vue component, breakpoint utilities, & fix toolbar styles when representing an anchor

--- packages/vue/demo/Demo.vue
+++ packages/vue/demo/Demo.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
 import IrToolbar from '../src/components/toolbar/IrToolbar.vue';
 import IrToolbarItem from '../src/components/toolbar/IrToolbarItem.vue';
-import IrToolbarBrand from "../src/components/toolbar/IrToolbarBrand.vue";
+import IrToolbarBrand from '../src/components/toolbar/IrToolbarBrand.vue';
+import IrContainer from '../src/components/IrContainer.vue';
+import IrButton from '../src/components/IrButton.vue';
 </script>
 
 <template>
-  <ir-toolbar>
+  <ir-toolbar tag='nav'>
     <template v-slot:start>
       <ir-toolbar-brand>
         Demo App
       </ir-toolbar-brand>
     </template>
-    <ir-toolbar-item active>Home</ir-toolbar-item>
+    <ir-toolbar-item tag='a' href='#' active>Home</ir-toolbar-item>
     <ir-toolbar-item>About</ir-toolbar-item>
   </ir-toolbar>
+
+  <ir-container>
+    <ir-button>
+      Default Button
+    </ir-button>
+
+    <ir-button color='primary'>
+      Primary Button
+    </ir-button>
+  </ir-container>
 </template>
 
 
-<style scoped></style>
+<style scoped lang="sass">
+</style>
---
+++ packages/vue/src/components/IrContainer.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+import { computed } from 'vue';
+
+export interface ContainerProps {
+  tag?: string | Component;
+  fluid?: boolean;
+}
+
+const props = withDefaults(defineProps<ContainerProps>(), {
+  tag: 'div',
+  fluid: false,
+});
+
+const dynamicClasses = computed(() => ({
+  fluid: props.fluid,
+}));
+</script>
+
+<template lang="pug">
+component.container(:is="props.tag" :class="dynamicClasses")
+  slot
+</template>
+
+<style scoped lang="sass"></style>
