6f2d185e7dee933f374d5e7d89b81f921cc55ede
Liz Ainslie <lizainslie16@gmail.com>
2022-07-18 08:25:14 -0700

    Grid system WIP

---
+++ packages/vue/src/components/grid/IrCol.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+import { computed } from 'vue';
+
+export interface ColProps {
+  tag?: string | Component;
+  reverse?: boolean;
+}
+
+const props = withDefaults(defineProps<ColProps>(), {
+  tag: 'div',
+  reverse: false,
+});
+
+const dynamicClasses = computed(() => ({
+  reverse: props.reverse,
+}));
+</script>
+
+<template lang="pug">
+component.col(:is="props.tag" :class="dynamicClasses")
+  slot
+</template>
+
+<style scoped lang="sass"></style>
---
+++ packages/vue/src/components/grid/IrRow.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+import { computed } from 'vue';
+
+export interface RowProps {
+  tag?: string | Component;
+  reverse?: boolean;
+}
+
+const props = withDefaults(defineProps<RowProps>(), {
+  tag: 'div',
+  reverse: false,
+});
+
+const dynamicClasses = computed(() => ({
+  reverse: props.reverse,
+}));
+</script>
+
+<template lang="pug">
+component.row(:is="props.tag" :class="dynamicClasses")
+  slot
+</template>
+
+<style scoped lang="sass"></style>
